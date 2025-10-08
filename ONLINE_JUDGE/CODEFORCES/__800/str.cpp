462d679451b4c41c0f76a990fc8358c67a506e86
labib <labib.hsn.bd@gmail.com>
2025-10-08 15:34:43 +0600

    update prb

--- ONLINE_JUDGE/CODEFORCES/__800/str.cpp
+++ ONLINE_JUDGE/CODEFORCES/__800/str.cpp
@@ -1,11 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Student{
+    public:
+        string name;
+        int age;
+        int cls;
+
+        Student(string name, int age, int cls){
+            this->name = name;
+            this->age = age;
+            this->cls = cls;
+        }
+};
+
 int main()
 {
-    string s = "LABIB";
-    sort(s.begin(), s.end());
 
-    cout << s << endl;
+    Student* std1 = new Student("Labib", 22, 8);
+
+    cout << std1->name << " " << std1->age << " " << std1->cls << endl;
+    // string s = "LABIB";
+    // sort(s.begin(), s.end());
+
+    // cout << s << endl;
     return 0;
 }
